67d156e43843d4b7c88c85c3d0a06debacd368ac
Mike Zornek <mike@clickablebliss.com>
2014-02-26 21:53:59 -0500

    initial commit

---
+++ GoldCards/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GoldCards
+//
+//  Created by Michael Zornek on 2/26/14.
+//  Copyright (c) 2014 Michael Zornek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GoldCards/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GoldCards
+//
+//  Created by Michael Zornek on 2/26/14.
+//  Copyright (c) 2014 Michael Zornek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
