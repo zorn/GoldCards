2963dd988b9544ee8591e2983f6433d555e8e4f5
Mike Zornek <mike@clickablebliss.com>
2014-03-06 22:40:46 -0500

    First pass at an about view.

--- GoldCards/Source/GCMainMenuViewController.m
+++ GoldCards/Source/GCMainMenuViewController.m
@@ -7,8 +7,16 @@
 #import "GCMechanicListViewController.h"
 #import "GCAchievementListViewController.h"
 #import "GCQuestListViewController.h"
+#import "PBWebViewController.h"
+#import "PBSafariActivity.h"
+
+@interface GCMainMenuViewController ()
+@property (strong) NSURL *aboutPageURL;
+@end
 
 @implementation GCMainMenuViewController
+
+@synthesize aboutPageURL=_aboutPageURL;
 
 - (void)viewDidLoad
 {
@@ -34,7 +42,47 @@
     } else if ([segue.identifier isEqualToString:@"questList"]) {
         UIViewController *questListVC = [segue zorn_destinationViewControllerOfClass:[GCQuestListViewController class]];
         [(GCQuestListViewController *)questListVC setManagedObjectContext:self.managedObjectContext];
+    } else if ([segue.identifier isEqualToString:@"about"]) {
+        PBWebViewController * aboutVC = (PBWebViewController *)[segue zorn_destinationViewControllerOfClass:[PBWebViewController class]];
+        [aboutVC setURL:[self aboutPageURL]];
+        PBSafariActivity *activity = [[PBSafariActivity alloc] init];
+        aboutVC.applicationActivities = @[activity];
+        aboutVC.excludedActivityTypes = @[UIActivityTypeMail, UIActivityTypeMessage, UIActivityTypePostToWeibo];
+        aboutVC.showsNavigationToolbar = YES;
     }
+}
+
+- (void)setAboutPageURL:(NSURL *)aboutPageURL
+{
+    _aboutPageURL = aboutPageURL;
+}
+
+- (NSURL *)aboutPageURL
+{
+	if (!_aboutPageURL) {
+		// create a dynamic about page showing the current version
+		NSString *version = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"];
+        
+		// original source
+		NSString *path = [[NSBundle mainBundle] pathForResource:@"about" ofType:@"html"];
+		NSMutableString *aboutPageSource = [[NSMutableString alloc] initWithString:[NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil]];
+		
+		// swap version
+		[aboutPageSource replaceOccurrencesOfString:@"{{appVersion}}" withString:version options:NSCaseInsensitiveSearch range:NSMakeRange(0, [aboutPageSource length])];
+		
+		// set the URL to the logo image
+		NSString *pathToAboutImage = [[NSBundle mainBundle] pathForResource:@"iTunesArtwork" ofType:@"png"];
+		[aboutPageSource replaceOccurrencesOfString:@"{{pathToIcon}}" withString:[@"file://" stringByAppendingString:pathToAboutImage] options:NSCaseInsensitiveSearch range:NSMakeRange(0, [aboutPageSource length])];
+		
+		// store new about page
+		NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+		NSString *documentsDirectory = [paths objectAtIndex:0];
+		NSURL *urlToNewAbout = [NSURL fileURLWithPath:[documentsDirectory stringByAppendingFormat:@"/about.html"]];
+		[aboutPageSource writeToURL:urlToNewAbout atomically:YES encoding:NSUTF8StringEncoding error:nil];
+		
+		_aboutPageURL = urlToNewAbout;
+	}
+	return _aboutPageURL;
 }
 
 @end
